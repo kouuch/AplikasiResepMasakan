73a1eb571dbea5f6d9ee62da34fc75afa08834e8
kouuch <rifkyirsada2@gmail.com>
2024-11-23 15:11:16 +0800

    modifikasi gradient panel

--- src/costum/GradientPanel.java
+++ src/costum/GradientPanel.java
@@ -6,14 +6,12 @@ import java.awt.Graphics2D;
 import java.awt.GradientPaint;
 import javax.swing.JPanel;
 
-// Kelas GradientPanel yang mewarisi JPanel
 public class GradientPanel extends JPanel {
 
-    private Color color1; // Warna pertama
-    private Color color2; // Warna kedua
-    private boolean isVertical; // Arah gradien
+    private Color color1;
+    private Color color2;
+    private boolean isVertical;
 
-    // Konstruktor untuk mengatur warna dan arah gradien
     public GradientPanel(Color color1, Color color2, boolean isVertical) {
         this.color1 = color1;
         this.color2 = color2;
@@ -25,7 +23,6 @@ public class GradientPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        // Tentukan arah gradien
         int x1 = 0, y1 = 0, x2 = getWidth(), y2 = getHeight();
         if (isVertical) {
             x2 = 0; // Gradien vertikal
@@ -33,7 +30,6 @@ public class GradientPanel extends JPanel {
             y2 = 0; // Gradien horizontal
         }
 
-        // Buat gradien warna
         GradientPaint gradient = new GradientPaint(x1, y1, color1, x2, y2, color2);
         g2d.setPaint(gradient);
         g2d.fillRect(0, 0, getWidth(), getHeight());
