d89b764c6b59a6c62a13f13795e5eda16e0422c9
kouuch <rifkyirsada2@gmail.com>
2024-11-24 18:48:15 +0800

    Handle Tombol Batal

--- src/view/MasakanRumahan/FormEditResep.java
+++ src/view/MasakanRumahan/FormEditResep.java
@@ -235,7 +235,12 @@ public class FormEditResep extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        // Kembali ke form Daftarmasakan
+    javax.swing.JPanel parentPanel = (javax.swing.JPanel) this.getParent();
+    parentPanel.removeAll();
+    parentPanel.add(new Daftarmasakan());
+    parentPanel.revalidate();
+    parentPanel.repaint();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
