f9ce8bdd8c1656c08d090f9fd110591f92534f28
kouuch <rifkyirsada2@gmail.com>
2024-11-28 23:29:44 +0800

    perbaikan background

--- src/costum/RectangleBack1.java
+++ src/costum/RectangleBack1.java
@@ -15,7 +15,7 @@ public class RectangleBack1 extends JPanel {
     public RectangleBack1() {
         this.cornerRadius = 40; // Default cornerRadius
         this.backgroundColor = new Color(0, 255, 255, 150); // Default backgroundColor dengan transparansi
-        setOpaque(true); // Menyeting panel agar tetap opak (tidak transparan)
+        setOpaque(false); // Mengatur agar panel menjadi transparan
     }
 
     // Getter untuk cornerRadius
@@ -43,18 +43,17 @@ public class RectangleBack1 extends JPanel {
     // Method untuk menggambar panel dengan sudut melengkung dan backgroundColor
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);  // Memanggil paintComponent dari JPanel untuk menjaga latar belakang default
+        super.paintComponent(g);  // Memanggil super.paintComponent untuk memastikan tidak ada gangguan
 
         Graphics2D g2d = (Graphics2D) g;
 
-        // Mengatur transparansi hanya untuk backgroundColor roundRect
+        // Mengatur transparansi hanya untuk roundRect
         AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);  // Transparansi 50%
         g2d.setComposite(alphaComposite);
 
-        // Mengatur warna untuk roundRect dengan transparansi
-        g2d.setColor(backgroundColor);  // Menentukan warna untuk isi roundRect
-        g2d.fillRoundRect(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius);  // Gambar roundRect
-
-        // Jika Anda ingin menambahkan border atau efek lainnya, bisa ditambahkan di sini.
+        // Tidak perlu menggambar latar belakang karena panel sudah transparan
+        // Menggambar roundRect di atas panel transparan
+        g2d.setColor(backgroundColor);  // Mengatur warna untuk roundRect
+        g2d.fillRoundRect(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius);  // Gambar roundRect dengan warna transparan
     }
 }
--- src/view/Sayuran/SayuranView.java
+++ src/view/Sayuran/SayuranView.java
@@ -28,6 +28,7 @@ public class SayuranView extends javax.swing.JPanel {
 
         jPanel2 = new javax.swing.JPanel();
         rectangle4 = new costum.Rectangle();
+        rectangle1 = new costum.Rectangle();
         jLabel1 = new javax.swing.JLabel();
 
         setLayout(new java.awt.CardLayout());
@@ -50,9 +51,22 @@ public class SayuranView extends javax.swing.JPanel {
 
         jPanel2.add(rectangle4, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 40, 320, 80));
 
+        javax.swing.GroupLayout rectangle1Layout = new javax.swing.GroupLayout(rectangle1);
+        rectangle1.setLayout(rectangle1Layout);
+        rectangle1Layout.setHorizontalGroup(
+            rectangle1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        rectangle1Layout.setVerticalGroup(
+            rectangle1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        jPanel2.add(rectangle1, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 170, -1, -1));
+
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/backGsayuran.png"))); // NOI18N
         jLabel1.setText("jLabel1");
-        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 792, -1));
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 200, 792, -1));
 
         add(jPanel2, "card2");
     }// </editor-fold>//GEN-END:initComponents
@@ -61,6 +75,7 @@ public class SayuranView extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel2;
+    private costum.Rectangle rectangle1;
     private costum.Rectangle rectangle4;
     // End of variables declaration//GEN-END:variables
 }
