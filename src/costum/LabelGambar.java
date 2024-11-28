a836c8c12b9835f10e1e8edbaa880bd21ca83bb0
kouuch <rifkyirsada2@gmail.com>
2024-11-29 00:57:06 +0800

    nambah debugging

--- src/costum/LabelGambar.java
+++ src/costum/LabelGambar.java
@@ -11,7 +11,11 @@ public class LabelGambar extends JLabel {
     public LabelGambar(String imagePath) {
         // Muat gambar dari file
         ImageIcon icon = new ImageIcon(imagePath);  // Ganti dengan path gambar Anda
-        image = icon.getImage();
+        if (icon.getImageLoadStatus() == MediaTracker.ERRORED) {
+            System.out.println("Gambar tidak ditemukan atau gagal dimuat!");
+        } else {
+            image = icon.getImage();
+        }
     }
 
     // Override paintComponent untuk menggambar gambar pada label
@@ -19,8 +23,13 @@ public class LabelGambar extends JLabel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);  // Memanggil super untuk membersihkan label
 
-        // Menyesuaikan ukuran gambar dengan ukuran label
-        image = image.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);  // Mengubah ukuran gambar agar sesuai dengan ukuran label
-        g.drawImage(image, 0, 0, this);  // Gambar akan muncul di posisi (0, 0) pada label
+        // Jika gambar berhasil dimuat
+        if (image != null) {
+            // Menyesuaikan ukuran gambar dengan ukuran label
+            image = image.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);  // Mengubah ukuran gambar agar sesuai dengan ukuran label
+            g.drawImage(image, 0, 0, this);  // Gambar akan muncul di posisi (0, 0) pada label
+        } else {
+            System.out.println("Gambar kosong atau gagal dimuat.");
+        }
     }
 }
